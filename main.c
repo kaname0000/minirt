762e1285efc24d4fd8aee86c26ebf91357eda4bc
Okuda Kaname <okaname@c4r1s8.42tokyo.jp>
2025-05-04 20:43:46 +0900

    間違ってなかった

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: okaname <okaname@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 18:12:49 by okaname           #+#    #+#             */
-/*   Updated: 2025/05/01 21:29:07 by okaname          ###   ########.fr       */
+/*   Updated: 2025/05/04 20:42:51 by okaname          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,22 @@
 
 void	make_world(t_world *world)
 {
-	world->cameras = make_camera(vec_init(0, -5, -5), vec_init(0, 0, 1), 20,
+	t_vec	camera;
+	t_vec	sphere;
+
+	sphere = vec_init(0, 0, 5);
+	camera = vec_init(7, -1, -3);
+	world->cameras = make_camera(camera, vec_sub(sphere, camera), 70,
 			world->screen_width);
-	world->lights = make_light(vec_init(-5, -5, -5), 1, color_init(100, 100,
+	world->lights = make_light(vec_init(5, -10, -5), 1, color_init(100, 100,
 				100));
-	world->objects = make_sphere(vec_init(0, 0, 5), 2, color_init(100, 100,
-				100));
-	world->objects->next = make_sphere(vec_init(0, 0, 9), 2, color_init(255,
+	world->objects = make_sphere(sphere, 4, color_init(255, 100, 100));
+	world->objects->next = make_sphere(vec_init(0, 0, 9), 4, color_init(100,
 				100, 100));
+	world->objects->next->next = make_sphere(vec_init(0, 0, 13), 4,
+			color_init(100, 100, 255));
+	world->objects->next->next->next = make_sphere(vec_init(0, 0, 17), 4,
+			color_init(100, 255, 100));
 }
 
 int	main(void)
--- make_object/make_camera.c
+++ make_object/make_camera.c
@@ -6,7 +6,7 @@
 /*   By: okaname <okaname@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 19:03:59 by okaname           #+#    #+#             */
-/*   Updated: 2025/05/01 20:44:12 by okaname          ###   ########.fr       */
+/*   Updated: 2025/05/04 20:28:59 by okaname          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,12 @@ t_camera	*make_camera(t_vec pos, t_vec dir, double fov, int width)
 	if (camera == NULL)
 		return (NULL);
 	camera->pos = pos;
-	camera->dir = dir;
+	camera->dir = vec_normalize(dir);
 	camera->fov = fov;
-	camera->sx = vec_normalize(vec_init(1, 0, -(dir.x / dir.z)));
-	camera->sy = vec_normalize(vec_cross(dir, camera->sx));
-	camera->view = vec_mult(dir, (width / 2) / tan((fov / 2) * (3.14 / 180)));
+	camera->sx = vec_normalize(vec_init(1, 0, -(camera->dir.x
+					/ camera->dir.z)));
+	camera->sy = vec_normalize(vec_cross(camera->dir, camera->sx));
+	camera->view = vec_mult(camera->dir, (width / 2) / tan((fov / 2) * (3.14
+					/ 180)));
 	return (camera);
 }
